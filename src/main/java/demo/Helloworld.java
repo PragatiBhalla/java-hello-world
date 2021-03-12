bd20036e07d43c4527423b2e873083f581ad9894
PragatiBhalla <51440499+PragatiBhalla@users.noreply.github.com>
2021-03-12 08:07:43 +0000

    Update Helloworld.java

--- src/main/java/demo/Helloworld.java
+++ src/main/java/demo/Helloworld.java
@@ -3,7 +3,7 @@ package demo;
 public class Helloworld {
 
 	public static boolean main(final String[] args) {
-		System.out.println("Hello, World!!!! The current time is ");
+		System.out.println("Hello, World!!");
 		return true;
 	}
 
