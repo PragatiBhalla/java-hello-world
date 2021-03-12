51b2ff5fb5ac81841d5719cbde80a8789ded4c19
PragatiBhalla <51440499+PragatiBhalla@users.noreply.github.com>
2021-03-12 15:05:52 +0000

    Update Helloworld.java

--- src/main/java/demo/Helloworld.java
+++ src/main/java/demo/Helloworld.java
@@ -5,9 +5,12 @@ import org.slf4j.LoggerFactory;
 
 public class Helloworld {
 	private static final Logger logger = LoggerFactory.getLogger(Helloworld.class);
-	public static boolean main(final String[] args) {
+	public boolean main() {
 		logger.log("Hello, World!!");
 		return true;
+	}
+	private Helloworld {
+		//sonarfix
 	}
 
 }
